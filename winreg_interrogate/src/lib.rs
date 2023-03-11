733bf62ec720e018c1d84171d4cf3b4ef6ff5292
Lars-Kristian Svenoy <lakrsv@gmail.com>
2023-03-11 17:19:55 +0000

    Remove winreg dependency

--- winreg_interrogate/src/lib.rs
+++ winreg_interrogate/src/lib.rs
@@ -1,6 +1,4 @@
 use std::path::PathBuf;
-use winreg::RegKey;
 
 pub fn test(){
-    dbg!(RegKey::load_app_key(PathBuf::from("C://Users/lakrs/Documents/CreativeWork/RustProjects/winreg_util/export-test/HKEY_LOCAL_MACHINE-SOFTWARE.reg"), false));
 }
